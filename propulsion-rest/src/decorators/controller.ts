b4adb27663111821be1ff7c0b6fd20e7a8329685
Merlin Reichwald <merlin.reichwald@gmail.com>
2019-10-09 01:22:17 +0200

    make controller only work on constructor-arg-less classes

--- propulsion-rest/src/decorators/controller.ts
+++ propulsion-rest/src/decorators/controller.ts
@@ -2,13 +2,14 @@ import {getRestApp, createControllerRouter} from "@/express";
 import {CONTROLLER} from "@/meta";
 import {HandlerOptions} from "@/types";
 import "reflect-metadata";
+import {NoArgsConstructor} from "@propero/propulsion-core";
 
 
 export function Controller(options: string | HandlerOptions) {
   if (typeof options == "string")
     options = { path: options || "/" };
 
-  return function (target: any) {
+  return function (target: NoArgsConstructor) {
     Reflect.defineMetadata(CONTROLLER, options, target);
     getRestApp().use((options as HandlerOptions).path!, createControllerRouter(target, options as HandlerOptions, new target()));
     return target;
