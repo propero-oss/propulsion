276bc03e7cff00c04d68a92a2649b224a70217b2
Merlin Reichwald <merlin.reichwald@gmail.com>
2019-10-09 01:15:00 +0200

    make request body only work on middleware-ish functions

--- propulsion-rest/src/decorators/request-body.ts
+++ propulsion-rest/src/decorators/request-body.ts
@@ -1,4 +1,4 @@
-import {joinMiddleware} from "@/express";
+import {joinMiddleware, MiddlewareFunction} from "@/express";
 import {getParser} from "@/parser";
 import {Request, Response, NextFunction, BodyParser} from "@/types";
 
@@ -6,11 +6,11 @@ import {Request, Response, NextFunction, BodyParser} from "@/types";
 export function RequestBody(parser: string | BodyParser) {
   const parse: BodyParser = typeof parser == "string" ? getParser(parser) : parser;
 
-  return function(target: any, key: string | symbol, desc: PropertyDescriptor) {
+  return function(target: any, key: string | symbol, desc: TypedPropertyDescriptor<MiddlewareFunction>) {
     const { value: orig } = desc;
 
-    desc.value = function(req: Request, res: Response, next: NextFunction) {
-      return joinMiddleware(parse, orig.bind(this))(req, res, next);
+    desc.value = function(req, res, next) {
+      return joinMiddleware(parse, orig!.bind(this))(req, res, next);
     };
 
     return desc;
