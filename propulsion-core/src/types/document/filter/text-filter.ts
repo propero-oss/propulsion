ae44fba2337f9f97025680f61005c7c03d26c437
Merlin Reichwald <merlin.reichwald@gmail.com>
2019-10-12 05:30:42 +0200

    fix long line

--- propulsion-core/src/types/document/filter/text-filter.ts
+++ propulsion-core/src/types/document/filter/text-filter.ts
@@ -1,4 +1,5 @@
-export type TextOperator = "contains" | "not-contains" | "match" | "not-match" | "starts-with" | "ends-with" | "not-starts-with" | "not-ends-with";
+export type TextOperator = "contains" | "not-contains" | "match" | "not-match"
+  | "starts-with" | "ends-with" | "not-starts-with" | "not-ends-with";
 
 
 export interface TextFilter<T, F extends keyof T = any> {
